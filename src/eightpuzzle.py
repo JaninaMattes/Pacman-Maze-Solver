cb97da343f118bb614f93701d0fba10ccad745fd
Janina Mattes <janina.alica.mattes@gmail.com>
2024-04-30 21:01:39 +0300

    src: Add eightpuzzle file

--- src/eightpuzzle.py
+++ src/eightpuzzle.py
@@ -1,8 +1,7 @@
 # eightpuzzle.py
-# --------------
+# ----------------
 # Licensing Information:  You are free to use or extend these projects for
-# educational purposes provided that (1) you do not distribute or publish
-# solutions, (2) you retain this notice, and (3) you provide clear
+# educational purposes provided that you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
 # 
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
@@ -11,9 +10,9 @@
 # Student side autograding was added by Brad Miller, Nick Hay, and
 # Pieter Abbeel (pabbeel@cs.berkeley.edu).
 
-
-import search
 import random
+
+import search.search as search
 
 # Module Classes
 
